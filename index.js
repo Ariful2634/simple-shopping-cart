cdf33e6b188d3cdb5a0f821b322c168f76d8f972
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-09-04 18:56:03 +0600

    done

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
-const cart = () =>{
+const cart = () => {
     // console.log('click')
     const pro = document.getElementById('product');
     const quan = document.getElementById('quantity');
     const product = pro.value;
     const quantity = quan.value;
-    pro.value='';
-    quan.value='';
+    pro.value = '';
+    quan.value = '';
     // console.log(product,quantity)
     const set = document.getElementById('set');
     const li = document.createElement('li');
-    li.innerText=`${product}: ${quantity}`
+    li.innerText = `${product}: ${quantity}`
     console.log(li)
     set.appendChild(li)
+    setStored(product, quantity)
 
 }
+
+// local Storage
+
+
+const store = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart)
+    }
+    return cart;
+}
+store()
+
+const setStored = (product, quantity) => {
+    const cart = store();
+    cart[product] = quantity;
+    const cartString = JSON.stringify(cart);
+    localStorage.setItem('cart', cartString)
+}
+
+const display=()=>{
+    const get = store();
+    for(const product in get){
+        const quantity=get[product]
+        console.log(product,quantity)
+        const set = document.getElementById('set');
+        const li = document.createElement('li');
+        li.innerText = `${product}: ${quantity}`
+        console.log(li)
+        set.appendChild(li)
+        setStored(product, quantity)
+    }
+}
+
+display()
+
